8c8b628114caa6a7faaad3ca53460553d934c1fe
simplyjoshardals <joshardalsgates@gmail.com>
2026-07-18 19:13:47 +0100

    updated th themeColor on the viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,11 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
+
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 const themeInitScript = `
